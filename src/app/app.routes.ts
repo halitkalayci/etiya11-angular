ce90c513ecf0fa5f571440ca5d276ca3b317a1af
halitklyc <halitenes.kalayci@gmail.com>
2026-07-16 16:16:09 +0300

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './pages/homepage/homepage';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+// Yalnızca route tanımları, /about -> şu component, /homepage -> şu component / ->
+export const routes: Routes = [
+  {path: '', redirectTo: 'homepage', pathMatch: 'full'}, // localhost:4200
+  {path:'homepage', component:Homepage},
+  {path:'about', component: About}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,35 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  btnClass = "bg-blue-500"
-  title: String = "Typescriptten gelen değer"; // Angular karışamaz
-  title2 = signal("Typescriptten gelen değer2"); // Anguları çağırıp
-  //değişken tanımla angular arkadaki işlemlerini kendi yapabilir.
-  name: String= "halit";
-  surname: String = "Kalaycı";
-
-  degerDegistir(): void {
-    this.title = "Title Değişti"
-    this.title2.set("Title2 Değişti")
-  }
-
-  isimDegistir() {
-    this.name = "Ahmet"
-    this.surname = "Kalaycı2"
-  }
-
-  isimDegistir2(event:any){
-    this.name=event.target.value;
-  }
-
-  konsolaYaz() {
-    console.log(this.name)
-  }
 }
