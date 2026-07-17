535b08381b9ec53d99add5ce133a2d62261d162d
halitklyc <halitenes.kalayci@gmail.com>
2026-07-17 11:01:25 +0300

    add iteration

--- src/app/pages/homepage/homepage.ts
+++ src/app/pages/homepage/homepage.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Todo } from '../../models/todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
 export class Homepage {
-  todo:Todo[] = [
-    {userId:1, id:1, title:'abc', completed:false},
-    {userId:2, id:2, title:'bqwrbqwrnq', completed:true},
+  todos:Todo[] = [
+    {userId:1, id:1, title:'abc', completed:false}, 
+    {userId:2, id:2, title:'def', completed:true},
+    {userId:3, id:3, title:'nrqwn', completed:false},
   ]
 }
