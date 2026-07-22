6d7340ec23700c09fd9d43d31a722b2ba5e2286c
halitklyc <halitenes.kalayci@gmail.com>
2026-07-22 10:44:49 +0300

    add env

--- src/app/services/todo-service.ts
+++ src/app/services/todo-service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/todo';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'; // eğer development'ta başlıyorsa angular.json bunu replace edecek.
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-
-  constructor(private httpClient:HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   fetchTodos(): Observable<Todo[]> {
-      return this.httpClient.get<Todo[]>("https://jsonplaceholder.typicode.com/todos");
+    console.log(environment.BASE_API_URL);
+    return this.httpClient.get<Todo[]>(`${environment.BASE_API_URL}todos`);
   }
 }
